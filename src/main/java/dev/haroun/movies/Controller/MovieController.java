df1a79cbe0a05aefce4b4937b345912220e6367a
haroun08 <129984358+haroun08@users.noreply.github.com>
2024-06-10 02:36:27 +0100

    final project

---
+++ src/main/java/dev/haroun/movies/Controller/MovieController.java
@@ -0,0 +1,37 @@
+package dev.haroun.movies.Controller;
+
+import dev.haroun.movies.MovieService;
+import dev.haroun.movies.model.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
+    }
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Movie> getSingleMovie(@PathVariable String imdbId) {
+        Optional<Movie> movie = movieService.singleMovie(imdbId);
+        if (movie.isPresent()) {
+            return new ResponseEntity<>(movie.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
---
+++ src/main/java/dev/haroun/movies/Repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package dev.haroun.movies.Repository;
+
+import dev.haroun.movies.model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
